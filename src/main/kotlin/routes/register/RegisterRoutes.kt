13b9e8056bbafcec260d3482a4217ae6f0a0c018
Mikhail Salnikov <salnikovmishanew@gmail.com>
2025-11-29 15:08:20 +0300

    Created rout for registration

---
+++ src/main/kotlin/routes/register/RegisterRoutes.kt
@@ -0,0 +1,39 @@
+package routes.register
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+import models.dto.register.RegisterRequest
+import models.dto.register.RegisterResponse
+import models.validation.register.RegisterValidation
+import repository.register.RegisterRepository
+import org.koin.ktor.ext.get
+
+fun Application.registerRoutes() {
+
+    val registerRepo = get<RegisterRepository>()
+    val registerValidation = get<RegisterValidation>()
+
+    routing {
+        post("/register") {
+
+            val dto = call.receive<RegisterRequest>() // DTO из тела запроса
+            val hasUser = registerValidation.hasUser(dto)
+
+            if (hasUser)
+                call.respond(
+                    HttpStatusCode.Conflict,
+                    RegisterResponse(
+                        "User with username = ${dto.username} and ${dto.email} is already exists"
+                    )
+                )
+            else {
+                val userId = registerRepo.create(dto)
+                call.respond(HttpStatusCode.Created, RegisterResponse("User with id = $userId is registered"))
+            }
+        }
+    }
+}
