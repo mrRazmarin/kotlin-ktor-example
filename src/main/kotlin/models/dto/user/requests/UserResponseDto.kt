2ffa1826d7a68d9ce5a6f70b9ed6aeb0f111bc0c
Mikhail Salnikov <salnikovmishanew@gmail.com>
2025-11-29 15:03:06 +0300

    create response dto for user entity

---
+++ src/main/kotlin/models/dto/user/requests/UserResponseDto.kt
@@ -0,0 +1,11 @@
+package models.dto.user.requests
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserResponseDto(
+    val id: Long,
+    override val username: String,
+    override val email: String,
+    val password: String
+) : IUserBaseRequestDto
