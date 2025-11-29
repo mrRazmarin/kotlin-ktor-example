4e2f7666082e26c81429da2e62938561270fd60a
Mikhail Salnikov <salnikovmishanew@gmail.com>
2025-11-29 14:58:23 +0300

    Added 2 singletons in DI: 1. repo for Registration 2. validator for Registration

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -2,12 +2,18 @@ package di
 
 import config.AppConfig
 import db.DatabaseFactory
+import models.validation.register.RegisterValidation
+import models.validation.register.RegisterValidationImpl
 import org.koin.dsl.module
-import repository.UserRepository
-import repository.UserRepositoryImpl
+import repository.register.RegisterRepository
+import repository.register.RegisterRepositoryImpl
+import repository.user.UserRepository
+import repository.user.UserRepositoryImpl
 
 val appModule = module {
     single { AppConfig(get()) } // Конфиг приложения через DI
     single <UserRepository> { UserRepositoryImpl() } // интерфейс через реализацию
     single { DatabaseFactory } // Singleton для работы с базой
+    single <RegisterRepository> { RegisterRepositoryImpl() } // Добавляем репу в DI
+    single <RegisterValidation>{ RegisterValidationImpl() }
 }
