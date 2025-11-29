148de116aa939b8a062ae0a139ee145f1cdcc989
Mikhail Salnikov <salnikovmishanew@gmail.com>
2025-11-29 14:59:38 +0300

    Change type variable in methods

--- src/main/kotlin/models/validation/user/UserValidation.kt
+++ src/main/kotlin/models/validation/user/UserValidation.kt
@@ -1,6 +1,6 @@
 package models.validation.user
 
-import models.dto.user.IUserBaseDto
+import models.dto.user.requests.IUserBaseRequestDto
 import models.validation.AbstractValidator
 
 class UserValidation : AbstractValidator() {
@@ -8,18 +8,18 @@ class UserValidation : AbstractValidator() {
     private val MAX_LENGTH: Int = 25
     private val MIN_LENGTH: Int = 3
 
-    fun hasMaxLength(dto: IUserBaseDto): String =
+    fun hasMaxLength(dto: IUserBaseRequestDto): String =
         if (!hasMaxLength(dto.username, MAX_LENGTH))
             "Username must be less than $MAX_LENGTH characters long"
         else ""
 
-    fun hasMinLength(dto: IUserBaseDto): String =
+    fun hasMinLength(dto: IUserBaseRequestDto): String =
         if (!hasMinLength(dto.username, MIN_LENGTH))
             "Username must be more than $MIN_LENGTH characters long"
         else ""
 
     // Общий метод: собираем все ошибки в список
-    fun validateAll(dto: IUserBaseDto): List<String> =
+    fun validateAll(dto: IUserBaseRequestDto): List<String> =
         listOf(
             hasMaxLength(dto),
             hasMinLength(dto)
