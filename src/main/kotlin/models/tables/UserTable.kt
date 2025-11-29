3fee69ee1fe7f37cc2420beb97d1ef32903a311a
Mikhail Salnikov <salnikovmishanew@gmail.com>
2025-11-29 14:59:59 +0300

    Added field for user table

--- src/main/kotlin/models/tables/UserTable.kt
+++ src/main/kotlin/models/tables/UserTable.kt
@@ -7,6 +7,7 @@ object Users : Table() {
     val id = long("id").autoIncrement()
     val username = varchar("username", 50)
     val email = varchar("email", 100)
+    val password = varchar("password", 50)
 
     override val primaryKey = PrimaryKey(id)
 }
